1b80d77b070531d5dd35d4dc85401ea8fbdce78d
Hector Mendoza <hectormendoza@byui.edu>
2018-11-13 18:04:03 -0500

    Video implemented on plane node. Anchor removed after image tracked is no longer recognized. Videos added.

--- 3DNews/ViewController.swift
+++ 3DNews/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
-    let videoNode = SKVideoNode(fileNamed: "video1.mp4")
+    //Global videoNodes to for future pause() & resume() options
+    //let videoNode = SKVideoNode(fileNamed: "video1.mp4")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,26 +46,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
         let node = SCNNode()
         
         if let imageAnchor = anchor as? ARImageAnchor {
             
             let planeNode = createPlaneNode(with: imageAnchor)
             
-            
-            //let videoNode = SKVideoNode(fileNamed: "video1.mp4")
-            
-            //videoNode.play()
-            
-            //let videoScene = SKScene(size: CGSize(width: 480, height: 360))
-            
-            //videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
-            
-            //videoNode.yScale = -1.0
-            
-            //videoScene.addChild(videoNode)
-            
-            //let planeNode = createPlaneNode(with: imageAnchor, video: videoScene)
+            if let imageName = imageAnchor.referenceImage.name {
+                
+                createVideoScene(with: imageName, at: planeNode)
+            }
             
             node.addChildNode(planeNode)
         }
@@ -73,8 +65,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        sceneView.session.remove(anchor: anchor)
+    }
+    
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        //videoNode.pause()
     }
     
     
@@ -83,12 +79,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
         
-        plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+        //Invisible plane this time
+        //plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
         
         let planeNode = SCNNode(geometry: plane)
         planeNode.eulerAngles.x = -.pi/2
         
         return planeNode
+    }
+    
+    
+    //MARK: - Creates a video scene on plane node
+    func createVideoScene(with imageName: String, at planeNode: SCNNode) {
+        
+        let videoNode = SKVideoNode(fileNamed: "\(imageName).mp4")
+        let videoScene = SKScene(size: CGSize(width: 640, height: 360))
+        videoNode.play()
+        
+        videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+        videoNode.yScale = -1.0
+        
+        videoScene.addChild(videoNode)
+        planeNode.geometry?.firstMaterial?.diffuse.contents = videoScene
     }
     
 }
