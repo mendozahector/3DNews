70b4dac99c87d8908f7f166c09b063bf57da36a0
Hector Mendoza <hectormendoza@byui.edu>
2018-11-13 17:05:01 -0500

    2 images tracked. Plane created on image recognition.

--- 3DNews/ViewController.swift
+++ 3DNews/ViewController.swift
@@ -19,34 +19,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        // Create a session configuration
+        
         let configuration = ARImageTrackingConfiguration()
         
-        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "News", bundle: Bundle.main) {
-            configuration.trackingImages = imageToTrack
+        if let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "News", bundle: Bundle.main) {
             
-            configuration.maximumNumberOfTrackedImages = 1
-            
-            print("Images Successfully Added")
+            configuration.trackingImages = referenceImages
+            configuration.maximumNumberOfTrackedImages = 2
         }
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+        
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
 
@@ -56,42 +48,47 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let node = SCNNode()
         
         if let imageAnchor = anchor as? ARImageAnchor {
+            
+            let planeNode = createPlaneNode(with: imageAnchor)
+            
+            
             //let videoNode = SKVideoNode(fileNamed: "video1.mp4")
             
-            videoNode.play()
+            //videoNode.play()
             
-            let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+            //let videoScene = SKScene(size: CGSize(width: 480, height: 360))
             
-            videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+            //videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
             
-            videoNode.yScale = -1.0
+            //videoNode.yScale = -1.0
             
-            videoScene.addChild(videoNode)
+            //videoScene.addChild(videoNode)
             
-            let planeNode = createPlaneNode(with: imageAnchor, video: videoScene)
+            //let planeNode = createPlaneNode(with: imageAnchor, video: videoScene)
             
             node.addChildNode(planeNode)
-            
-            //            if let imageName = imageAnchor.referenceImage.name {
-            //                createPokeScene(with: imageName, at: planeNode)
-            //            }
         }
         
         return node
     }
     
+    
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        videoNode.pause()
+        //videoNode.pause()
     }
     
-    func createPlaneNode(with imageAnchor: ARImageAnchor, video videoScene: SKScene) -> SCNNode {
+    
+    //MARK: - Creates an invisible plane node on tracked image
+    func createPlaneNode(with imageAnchor: ARImageAnchor) -> SCNNode {
+        
         let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
         
-        plane.firstMaterial?.diffuse.contents = videoScene
+        plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
         
         let planeNode = SCNNode(geometry: plane)
         planeNode.eulerAngles.x = -.pi/2
         
         return planeNode
     }
+    
 }
